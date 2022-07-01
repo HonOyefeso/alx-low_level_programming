bf1ac39b18f8c5af81049253343b2c1bc8ceeeb5
HonOyefeso <oyefesomathmarvell@gmail.com>
2022-06-30 19:49:26 -0700

    trial

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#!/bash/bash
+#include <stdio.h>/** * main - Entry point* Return: Always 0 (success)*/int main(void)
+{
+	int i;
+	ong int l;
+	long long int ll;
+	float d;
+	char c;
+
+	printf("Size of a char: %lubyte(s)\n", sizeof(c))
+	printf("Size of an int: %lubyte(s)\n", sizeof(i))
+	printf("Size of a long int:%lu byte(s)\n", sizeof(l));
+	printf("Size of a long longint: %lu byte(s)\n", sizeof(ll));printf("Size of a float: %lubyte(s)\n", sizeof(d));eturn (0)}
